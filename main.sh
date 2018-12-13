8dc3f7f57e2a89912427e1675f10969c49c1334c
jose <zecapistolas@gmail.com>
2018-12-13 12:42:14 -0800

    Improved fl execution and sanity checks.

--- main.sh
+++ main.sh
@@ -1,10 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# 
+# This script runs GZoltar on a specified D4J project/bug using manually written.
+# 
+# Usage:
+# ./main.sh <pid> <bid>
+# 
+# Parameters:
+# - <pid>  Defects4J's project ID: Chart, Closure, Lang, Math, Mockito or Time.
+# - <bid>  Bug ID
+# 
+# Examples:
+# 
+# - Executing GZoltar on bug Chart-5 and using manually written tests
+#   $ ./main.sh Chart 5
+# 
+# Environment variables:
+# - JAVA_HOME  Needs to be set and must point to the Java installation.
+# - ANT_HOME   Needs to be set and must point to the Ant installation.
+# - D4J_HOME   Needs to be set and must point to the Defects4J installation.
+# 
+# ------------------------------------------------------------------------------
 set -e # makes the shell to fail if any command returns a non-zero status
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 source "$SCRIPT_DIR/utils.sh" || exit 1
 
-# -------------------------------------------------------- Envs & Args
+# ------------------------------------------------------------------ Envs & Args
 
 # Check whether JAVA_HOME is set
 [ "$JAVA_HOME" != "" ] || die "[ERROR] JAVA_HOME is not set!"
@@ -13,104 +35,98 @@ source "$SCRIPT_DIR/utils.sh" || exit 1
 # Check whether D4J_HOME is set
 [ "$D4J_HOME" != "" ] || die "[ERROR] D4J_HOME is not set!"
 
+export PATH="$JAVA_HOME/bin:$ANT_HOME/bin:$PATH"
+
 USAGE="Usage: $0 <pid> <bid>"
-[ $# -eq 2 ] || die "$USAGE";
+[ $# -eq 2 ] || die "$USAGE"
 PID="$1"
 BID="$2"
 
-export PATH="$JAVA_HOME/bin:$ANT_HOME/bin:$PATH"
-
-# --------------------------------------------------------------- Main
+# --------------------------------------------------------------------------- FL
 
 echo "PID: $$"
 hostname
 java -version
 ant -version
+python --version
 
 echo ""
 echo "[INFO] Checkout $PID-${BID}b"
-tmp_dir=$(_checkout "$PID" "$BID" "b");
+tmp_dir=$(_checkout "$PID" "$BID" "b")
 if [ $? -ne 0 ]; then
-  echo "[ERROR] Checkout of the BUGGY version has failed!"
+  echo "[ERROR] Checkout of the $PID-${BID}b version has failed!"
   rm -rf "$tmp_dir"
-  exit 1;
+  exit 1
 fi
 
 echo ""
-echo "[INFO] Compile $PID-${BID}b"
-_compile "$tmp_dir"
+echo "[INFO] Run GZoltar on $PID-${BID}b"
+echo "[INFO] Start: $(date)"
+_fault_localization "$PID" "$BID" "$tmp_dir"
 if [ $? -ne 0 ]; then
-  echo "[ERROR] Compilation of $PID-${BID}b has failed!"
+  echo "[ERROR] Execution of GZoltar on $PID-${BID}b has failed!"
   rm -rf "$tmp_dir"
-  exit 1;
+  exit 1
 fi
+echo "[INFO] End: $(date)"
 
-# ------------------------------------ Run manually-written test cases
+# ---------------------------------------------------------------- Sanity checks
 
-echo ""
-echo "[INFO] Running relevant developer-written test cases with GZoltar on $PID-${BID}b"
-"$D4J_HOME/framework/bin/defects4j" fault-localization -w "$tmp_dir" -y sfl -e ochiai -g line
+TESTS_FILE="$tmp_dir/tests"
+SPECTRA_FILE="$tmp_dir/spectra"
+MATRIX_FILE="$tmp_dir/matrix"
+SOURCE_CODE_LINES_FILE="$tmp_dir/source_code_lines.txt"
 
-if [ $? -ne 0 ]; then
-  echo "[ERROR] Execution of all manually-written test cases with GZoltar on $PID-${BID}b has failed!"
-  echo "[DEBUG] Log:"
-  cat "$tmp_dir/.fault-localization.log"
-  rm -rf "$tmp_dir"
-  exit 1;
-fi
-
-if [ ! -s "$tmp_dir/gzoltar.ser" ]; then
-  echo "[ERROR] File '$tmp_dir/gzoltar.ser' does not exit or it is empty!"
-  rm -rf "$tmp_dir"
-  exit 1;
-elif [ ! -s "$tmp_dir/sfl/txt/matrix.txt" ]; then
-  echo "[ERROR] File '$tmp_dir/sfl/txt/matrix.txt' does not exit or it is empty!"
-  rm -rf "$tmp_dir"
-  exit 1;
-elif [ ! -s "$tmp_dir/fault_localization.csv" ]; then
-  echo "[ERROR] File '$tmp_dir/fault_localization.csv' does not exit or it is empty!"
-  rm -rf "$tmp_dir"
-  exit 1;
-fi
-
-# ------------------------------------------------------ Sanity checks
-
-TESTS_FILE="$tmp_dir/sfl/txt/tests.csv"
-SPECTRA_FILE="$tmp_dir/sfl/txt/spectra.csv"
-MATRIX_FILE="$tmp_dir/sfl/txt/matrix.txt"
-LINE_OCHIAI_RANKING_FILE="$tmp_dir/sfl/txt/line.ochiai.ranking.csv"
+_is_a_known_exception "$PID" "$BID"
+is_a_known_exception="$?" # 0 yes, 1, no, it is not
 
 echo ""
 echo "[INFO] Running a few sanity checks on $PID-${BID}b"
+echo ""
 
-## 1. Does GZoltar and D4J agree on the number of triggering test cases?
+## 1. Do GZoltar and D4J agree on the number of triggering test cases?
 
 num_triggering_test_cases_gzoltar=$(grep -a ",FAIL," "$TESTS_FILE" | wc -l)
 num_triggering_test_cases_d4j=$(grep -a "^--- " "$D4J_HOME/framework/projects/$PID/trigger_tests/$BID" | wc -l)
 
-if [ "$num_triggering_test_cases_gzoltar" -ne "$num_triggering_test_cases_d4j" ]; then
-  echo "[ERROR] Number of triggering test cases reported by GZoltar ($num_triggering_test_cases_gzoltar) is not the same as reported by D4J ($num_triggering_test_cases_d4j)!"
+if [ "$is_a_known_exception" == "0" ]; then
+  echo "[INFO] Do GZoltar and D4J agree on the number of triggering test cases? It is a know exception therefore check is not performed."
+elif [ "$num_triggering_test_cases_gzoltar" -ne "$num_triggering_test_cases_d4j" ]; then
+  echo "[ERROR] Do GZoltar and D4J agree on the number of triggering test cases? No, D4J: $num_triggering_test_cases_d4j vs GZoltar: $num_triggering_test_cases_gzoltar"
+  echo "[DEBUG] D4J triggering test cases:"
+  grep -a "^--- " "$D4J_HOME/framework/projects/$PID/trigger_tests/$BID" | cut -f2 -d' '
+  echo "[DEBUG] GZoltar triggering test cases:"
+  grep -a ",FAIL," "$TESTS_FILE" | cut -f1 -d','
   rm -rf "$tmp_dir"
-  exit 1;
+  exit 1
+else
+  echo "[INFO] Do GZoltar and D4J agree on the number of triggering test cases? Yes, D4J: $num_triggering_test_cases_d4j == GZoltar: $num_triggering_test_cases_gzoltar."
 fi
 
-## 2. Does GZoltar and D4J agree on the list of triggering test cases?
+## 2. Do GZoltar and D4J agree on the list of triggering test cases?
 
-agree=true
-while read -r trigger_test; do
-  class_test_name=$(echo "$trigger_test" | cut -f2 -d' ' | cut -f1 -d':')
-  unit_test_name=$(echo "$trigger_test" | cut -f2 -d' ' | cut -f3 -d':')
+if [ "$is_a_known_exception" == "0" ]; then
+  echo "[INFO] Do GZoltar and D4J agree on the list of triggering test cases? It is a know exception therefore check is not performed."
+else
+  agree=true
+  while read -r trigger_test; do
+    class_test_name=$(echo "$trigger_test" | cut -f2 -d' ' | cut -f1 -d':')
+    unit_test_name=$(echo "$trigger_test" | cut -f2 -d' ' | cut -f3 -d':')
 
-  # e.g., org.apache.commons.math.complex.ComplexTest#testMath221,FAIL,3111187,junit.framework.AssertionFailedError:
-  if ! grep -a -q "^$class_test_name#$unit_test_name,FAIL," "$TESTS_FILE"; then
-    echo "[ERROR] Triggering test case '$class_test_name#$unit_test_name' has not been reported by GZoltar!"
-    agree=false
+    # e.g., org.apache.commons.math.complex.ComplexTest#testMath221,FAIL,3111187,junit.framework.AssertionFailedError:
+    if ! grep -a -q -F "$class_test_name#$unit_test_name,FAIL," "$TESTS_FILE"; then
+      echo "[ERROR] Triggering test case '$class_test_name#$unit_test_name' has not been reported by GZoltar!"
+      agree=false
+    fi
+  done < <(grep -a "^--- " "$D4J_HOME/framework/projects/$PID/trigger_tests/$BID")
+
+  if [[ $agree == false ]]; then
+    echo "[ERROR] Do GZoltar and D4J agree on the list of triggering test cases? No."
+    rm -rf "$tmp_dir"
+    exit 1
+  else
+    echo "[INFO] Do GZoltar and D4J agree on the list of triggering test cases? Yes."
   fi
-done < <(grep -a "^--- " "$D4J_HOME/framework/projects/$PID/trigger_tests/$BID")
-
-if [[ $agree == false ]]; then
-  rm -rf "$tmp_dir"
-  exit 1;
 fi
 
 ## 3. Has the faulty class(es) been reported?
@@ -118,12 +134,11 @@ fi
 num_classes_not_reported=0
 modified_classes_file="$D4J_HOME/framework/projects/$PID/modified_classes/$BID.src"
 while read -r modified_class; do
-  class_name=$(echo "${modified_class%.*}\$${modified_class##*.}")
-  echo "[DEBUG] modified_class: $modified_class, class_name: $class_name"
-  if grep -q "^$class_name#" "$SPECTRA_FILE"; then
-    echo "[DEBUG] Has '$class_name' been reported? Yes."
+  echo "[DEBUG] modified_class: $modified_class"
+  if grep -q "^$modified_class#" "$SPECTRA_FILE"; then
+    echo "[DEBUG] Has '$modified_class' been reported? Yes."
   else
-    echo "[DEBUG] Has '$class_name' been reported? No."
+    echo "[DEBUG] Has '$modified_class' been reported? No."
     num_classes_not_reported=$((num_classes_not_reported+1))
   fi
 done < <(cat "$modified_classes_file")
@@ -131,15 +146,17 @@ done < <(cat "$modified_classes_file")
 if [ "$num_classes_not_reported" -eq "1" ] && [ "$PID" == "Mockito" ] && [ "$BID" == "19" ]; then
   # one of the modified classes of Mockito-19 is an interface without
   # any code. as interfaces with no code have no lines of code in bytecode,
-  # GZoltar does instrument it and therefore does not report it in the
-  # spectra file
+  # GZoltar does instrument it however it does not report it in the spectra file
   echo "Mockito-19 excluded from the check on the number of modified classes reported."
 elif [ "$num_classes_not_reported" -ne "0" ]; then
+  echo "[ERROR] Has the faulty class(es) been reported? No."
   rm -rf "$tmp_dir"
-  exit 1;
+  exit 1
 fi
 
-## 4. Has the faulty line(s) been covered by triggering test case(s)?
+echo "[INFO] Has the faulty class(es) been reported? Yes."
+
+## 4. Does spectra file include at least one buggy-line?
 
 buggy_lines_file="$D4J_HOME/framework/projects/$PID/buggy_lines/$BID.buggy.lines"
 num_buggy_lines=$(wc -l "$buggy_lines_file" | cut -f1 -d' ')
@@ -152,27 +169,105 @@ fi
 
 candidates_file="$D4J_HOME/framework/projects/$PID/buggy_lines/$BID.candidates"
 
-if [ "$num_buggy_lines" -ne "$num_unrankable_lines" ]; then
+if [ "$is_a_known_exception" == "0" ]; then
+  echo "[INFO] Does spectra file include at least one buggy-line? It is a know exception therefore check is not performed."
+elif [ "$num_buggy_lines" -ne "$num_unrankable_lines" ]; then
+
+  at_least_one_buggy_line_in_spectra_file=false
+  while read -r buggy_line; do
+    echo "[DEBUG] Buggy line: $buggy_line"
+    class_name=$(echo "$buggy_line" | cut -f1 -d'#' | sed 's/.java$//' | tr '/' '.')
+    line_number=$(echo "$buggy_line" | cut -f2 -d'#')
+
+    if grep -q ":$class_name#$line_number$" "$SOURCE_CODE_LINES_FILE"; then
+      echo "  [DEBUG] Buggy line '$class_name#$line_number' is part of another line"
+      buggy_line=$(grep ":$class_name#$line_number$" "$SOURCE_CODE_LINES_FILE" | cut -f1 -d':')
+      class_name=$(echo "$buggy_line" | cut -f1 -d'#')
+      line_number=$(echo "$buggy_line" | cut -f2 -d'#')
+    fi
+    echo "  [DEBUG] Class name: $class_name"
+    echo "  [DEBUG] Line number: $line_number"
+
+    if grep -q "^$class_name#$line_number$" "$SPECTRA_FILE"; then
+      echo "  [DEBUG] Buggy line $class_name#$line_number has been reported"
+      at_least_one_buggy_line_in_spectra_file=true
+      break # break this while loop, as we already know that it
+      # is not a false positive
+    fi
+  done < <(grep -v "FAULT_OF_OMISSION" "$buggy_lines_file")
+
+  if [[ $at_least_one_buggy_line_in_spectra_file == false ]]; then
+    # at this point, no buggy line has been found, try to find a
+    # suitable candidate line, if any
+    if [ -s "$candidates_file" ]; then
+      while read -r candidate_line; do
+        echo "[DEBUG] Candidate line: $candidate_line"
+        candidate=$(echo "$candidate_line" | cut -f2 -d',' | sed 's/.java#/#/' | tr '/' '.')
+
+        if grep -q ":$candidate$" "$SOURCE_CODE_LINES_FILE"; then
+          echo "  [DEBUG] Candidate line '$candidate' is part of another line"
+          candidate=$(grep ":$candidate$" "$SOURCE_CODE_LINES_FILE" | cut -f1 -d':')
+        fi
+        echo "  [DEBUG] '$candidate'"
+
+        if grep -q "^$candidate$" "$SPECTRA_FILE"; then
+          echo "  [DEBUG] Candidate line $candidate has been reported"
+          at_least_one_buggy_line_in_spectra_file=true
+          break # break this while loop, as we already know that it
+          # is not a false positive
+        fi
+      done < <(cat "$candidates_file")
+    else
+      echo "[DEBUG] Candidates file '$candidates_file' is empty or does not exist"
+    fi
+  fi
+
+  if [[ $at_least_one_buggy_line_in_spectra_file == false ]]; then
+    echo "[ERROR] Does spectra file include at least one buggy-line? No."
+    rm -rf "$tmp_dir"
+    exit 1
+  else
+    echo "[INFO] Does spectra file include at least one buggy-line? Yes."
+  fi
+else
+  echo "[INFO] Does spectra file include at least one buggy-line? Check cannot be performed as there is not any rankable line."
+fi
+
+## 5. Do all test cases cover at least one buggy-line?
+
+if [ "$is_a_known_exception" == "0" ]; then
+  echo "[INFO] Do all test cases cover at least one buggy-line? It is a know exception therefore check is not performed."
+elif [ "$num_buggy_lines" -ne "$num_unrankable_lines" ]; then
 
   # find out whether all failing test cases cover at least one buggy line
   while read -r test_coverage; do
     false_positive=true
 
     failing_test_id=$(echo "$test_coverage" | cut -f1 -d':')
-    echo "[INFO] Test case id '$failing_test_id'"
+    echo "[DEBUG] Test case id '$failing_test_id'"
 
-    test_cov_file="/tmp/$USER-test-method-coverage-$$.txt"
+    test_cov_file="$tmp_dir/$USER-test-$failing_test_id-covered-components-$$.txt"
     echo "$test_coverage" | cut -f2 -d':' | awk '{for (i = 1; i <= NF; ++i) if ($i == 1) print i}' > "$test_cov_file"
 
     while read -r buggy_line; do
-      echo "  [DEBUG] Buggy_line: $buggy_line"
-      java_file=$(echo "$buggy_line" | cut -f1 -d'#')
+      class_name=$(echo "$buggy_line" | cut -f1 -d'#' | sed 's/.java$//' | tr '/' '.')
       line_number=$(echo "$buggy_line" | cut -f2 -d'#')
-      echo "  [DEBUG] Java_file: $java_file"
-      echo "  [DEBUG] Line_number: $line_number"
 
-      if grep -q "^$java_file#$line_number," "$LINE_OCHIAI_RANKING_FILE"; then
-        echo "  [DEBUG] Test case id '$failing_test_id' covers => buggy line: '$java_file#$line_number'"
+      if grep -q ":$class_name#$line_number$" "$SOURCE_CODE_LINES_FILE"; then
+        buggy_line=$(grep ":$class_name#$line_number$" "$SOURCE_CODE_LINES_FILE" | cut -f1 -d':')
+        class_name=$(echo "$buggy_line" | cut -f1 -d'#')
+        line_number=$(echo "$buggy_line" | cut -f2 -d'#')
+      fi
+
+      if ! grep -q "^$class_name#$line_number$" "$SPECTRA_FILE"; then
+        continue
+      fi
+
+      spectra_id=$(grep -n "^$class_name#$line_number$" "$SPECTRA_FILE" | cut -f1 -d':')
+      echo "  [DEBUG] Spectra id of '$class_name#$line_number' is $spectra_id"
+
+      # Does test case id $failing_test_id touch this buggy line?
+      if grep -q "^$spectra_id$" "$test_cov_file"; then
         false_positive=false
         break # break this while loop, as we already know that it
         # is not a false positive
@@ -180,84 +275,52 @@ if [ "$num_buggy_lines" -ne "$num_unrankable_lines" ]; then
     done < <(grep -v "FAULT_OF_OMISSION" "$buggy_lines_file")
 
     if [[ $false_positive == true ]]; then
-      # at this point, no buggy line has been found, try to find a
-      # suitable candidate line, if any
+      # at this point, no buggy line has been covered, try to find a suitable
+      # candidate line, if any
       if [ -s "$candidates_file" ]; then
         while read -r candidate_line; do
-          echo "  [DEBUG] candidate_line: $candidate_line"
-          candidate=$(echo "$candidate_line" | cut -f2 -d',')
-          echo "  [DEBUG] candidate: $candidate"
+          candidate=$(echo "$candidate_line" | cut -f2 -d',' | sed 's/.java#/#/' | tr '/' '.')
 
-          if grep -q "^$candidate," "$LINE_OCHIAI_RANKING_FILE"; then
-            echo "  [DEBUG] Test case id '$failing_test_id' covers => candidate line: '$candidate'"
+          if grep -q ":$candidate$" "$SOURCE_CODE_LINES_FILE"; then
+            candidate=$(grep ":$candidate$" "$SOURCE_CODE_LINES_FILE" | cut -f1 -d':')
+          fi
+
+          if ! grep -q "^$candidate$" "$SPECTRA_FILE"; then
+            continue
+          fi
+
+          spectra_id=$(grep -n "^$candidate$" "$SPECTRA_FILE" | cut -f1 -d':')
+          echo "  [DEBUG] Spectra id of candidate '$candidate' is $spectra_id"
+
+          # Does test case id $failing_test_id touch this candidate line?
+          if grep -q "^$spectra_id$" "$test_cov_file"; then
             false_positive=false
             break # break this while loop, as we already know that it
             # is not a false positive
           fi
         done < <(cat "$candidates_file")
+      else
+        echo "[DEBUG] Candidates file '$candidates_file' is empty or does not exist"
       fi
     fi
-
-    ##
-    # Known exceptions
-
-    if [ "$PID" == "Closure" ] && [ "$BID" == "67" ]; then
-      #  ) {
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
-    if [ "$PID" == "Closure" ] && [ "$BID" == "114" ]; then
-      # } else {
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
-    if [ "$PID" == "Closure" ] && [ "$BID" == "119" ]; then
-      # case x:
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
-
-    if [ "$PID" == "Math" ] && [ "$BID" == "12" ]; then
-      # implements X
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
-    if [ "$PID" == "Math" ] && [ "$BID" == "104" ]; then
-      # private static final double DEFAULT_EPSILON = 10e-9;
-      # ^ private fields do not have a line number in bytecode
-      continue
-    fi
-
-    if [ "$PID" == "Lang" ] && [ "$BID" == "29" ]; then
-      # static float toJavaVersionInt(String version) {
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
-    if [ "$PID" == "Lang" ] && [ "$BID" == "56" ]; then
-      # private fields are not in the bytecode
-      # private Rule[] mRules;
-      # private int mMaxLengthEstimate;
-      # ^ the above lines do not have a line number in bytecode
-      continue
-    fi
-
-    if [ "$PID" == "Mockito" ] && [ "$BID" == "8" ]; then
-      # } else {
-      # ^ buggy line to which there is not a line number in bytecode
-      continue
-    fi
+    rm -f "$test_cov_file"
 
     if [[ $false_positive == true ]]; then
       echo "[ERROR] Test case id '$failing_test_id' does not cover any buggy line!"
       rm -rf "$tmp_dir"
-      exit 1;
+      exit 1
+    else
+      echo "[INFO] Do all test cases cover at least one buggy-line? Yes."
     fi
   done < <(grep -n " -$" "$MATRIX_FILE")
-
+else
+  echo "[INFO] Do all test cases cover at least one buggy-line? Check cannot be performed as there is not any rankable line."
 fi
 
-# do not leave anything behind
+# Clean up
 rm -rf "$tmp_dir"
+
+exit 0
 
 # EOF
 
--- utils.sh
+++ utils.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export TZ='America/Los_Angeles' # some D4J bugs requires this specific TimeZone
+PWD=`pwd`
+
+export MALLOC_ARENA_MAX=1 # Iceberg's requirement
+export TZ='America/Los_Angeles' # some D4J's requires this specific TimeZone
+
+export _JAVA_OPTIONS="-Xmx2048M -XX:MaxHeapSize=1024M"
+export MAVEN_OPTS="-Xmx1024M"
+export ANT_OPTS="-Xmx2048M -XX:MaxHeapSize=1024M"
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -29,8 +36,8 @@ _checkout() {
   local version="$3" # either b or f
 
   local output_dir="/tmp/$USER/gz-d4j/_$$-$pid-$bid"
-  rm -rf "$output_dir"; mkdir -p "$output_dir"
-
+  rm -rf "$output_dir"
+  mkdir -p "$output_dir"
   "$D4J_HOME/framework/bin/defects4j" checkout -p "$pid" -v "${bid}$version" -w "$output_dir" || return 1
 
   echo "$output_dir"
@@ -38,30 +45,173 @@ _checkout() {
 }
 
 ##
-# Compiles a D4J's project-bug.
+#
 ##
-_compile() {
-  local USAGE="Usage: _compile <project dir>"
-  if [ "$#" != 1 ]; then
+_fault_localization() {
+  local USAGE="Usage: _fault_localization <pid> <bid> <tmp_dir>"
+  if [ "$#" != 3 ]; then
     echo "$USAGE" >&2
     return 1
   fi
 
-  local project_dir="$1"
+  local pid="$1"
+  local bid="$2"
+  local tmp_dir="$3"
 
-  if [[ $project_dir == *"-Mockito-"* ]]; then
-    rm -f "$project_dir/buildSrc/src/test/groovy/testutil/OfflineCheckerTest.groovy" # this test case may cause the compilation to fail
+  "$D4J_HOME/framework/bin/defects4j" fault-localization \
+      -w "$tmp_dir" \
+      -y sfl \
+      -e ochiai \
+      -g line || return 1
 
-    echo "[DEBUG] Mockito project identified, export '$project_dir/.gradle-local-home' as the 'GRADLE_USER_HOME'" >&2
-    export GRADLE_USER_HOME="$project_dir/.gradle-local-home"
+  local spectra_file="$tmp_dir/sfl/txt/spectra.csv"
+  local matrix_file="$tmp_dir/sfl/txt/matrix.txt"
+  local tests_file="$tmp_dir/sfl/txt/tests.csv"
+  local source_code_lines_file="$tmp_dir/source_code_lines.txt"
+
+  if [ ! -s "$spectra_file" ]; then
+    echo "Spectra file '$spectra_file' is empty or does not exist" >&2
+    return 1
   fi
 
-  pushd . > /dev/null 2>&1
-  cd "$project_dir"
-    "$D4J_HOME/framework/bin/defects4j" compile || return 1
-  popd > /dev/null 2>&1
+  if [ ! -s "$matrix_file" ]; then
+    echo "Matrix file '$matrix_file' is empty or does not exist" >&2
+    return 1
+  fi
+
+  if [ ! -s "$tests_file" ]; then
+    echo "Tests file '$tests_file' is empty or does not exist" >&2
+    return 1
+  fi
+
+  if [ ! -f "$source_code_lines_file" ]; then
+    echo "Source code line  file '$source_code_lines_file' does not exist" >&2
+    return 1
+  fi
+
+  # Remove extension
+  mv "$spectra_file" $(dirname "$spectra_file")/$(basename "$spectra_file" .csv) || return 1
+  spectra_file=$(dirname "$spectra_file")/$(basename "$spectra_file" .csv)
+  mv "$matrix_file" $(dirname "$matrix_file")/$(basename "$matrix_file" .txt) || return 1
+  matrix_file=$(dirname "$matrix_file")/$(basename "$matrix_file" .txt)
+  mv "$tests_file" $(dirname "$tests_file")/$(basename "$tests_file" .csv) || return 1
+  tests_file=$(dirname "$tests_file")/$(basename "$tests_file" .csv)
+
+  # Remove header
+  tail -n +2 "$spectra_file" > "$spectra_file.tmp" && mv -f "$spectra_file.tmp" "$spectra_file" || return 1
+  tail -n +2 "$tests_file" > "$tests_file.tmp" && mv -f "$tests_file.tmp" "$tests_file" || return 1
+
+  # Backup
+  cp "$spectra_file" $(dirname "$spectra_file")/.spectra || return 1
+
+  # Remove inner class(es) names (as there is not a .java file for each one)
+  sed -i -E 's/(\$\w+)\$.*#/\1#/g' "$spectra_file" || return 1
+
+  # Remove method name of each row in the spectra file
+  sed -i 's/#.*:/#/g' "$spectra_file" || return 1
+
+  # Replace class name symbol
+  sed -i 's/\$/./g' "$spectra_file" || return 1
+
+  # Replace / by .
+  sed -i 's/\//./g' "$source_code_lines_file" || return 1
+  # Remove .java extension
+  sed -i 's/.java#/#/g' "$source_code_lines_file" || return 1
 
   return 0
+}
+
+##
+# Checks whether a sanity check on the coverage of each triggering test case of
+# a project-bug can or cannot be performed. A sanity check on a project-bug can
+# only be performed if and only if a buggy line / candidate line could be
+# identified in bytecode.
+##
+_is_a_known_exception() {
+  local USAGE="Usage: _is_a_known_exception <pid> <bid>"
+  if [ "$#" != 2 ]; then
+    echo "$USAGE" >&2
+    return 1
+  fi
+
+  local pid="$1"
+  local bid="$2"
+
+  if [ "$pid" == "Closure" ] && [ "$bid" == "67" ]; then
+    # there is not a bytecode instruction for the buggy line
+    return 0
+  fi
+
+  if [ "$pid" == "Closure" ] && [ "$bid" == "100" ]; then
+    # there are two different ways of triggering this bug: either covering the
+    # return statement (i.e., line number 146) or the lines annotated as a
+    # 'fault of omission'. If a test case does not execute the return statement
+    # it is impossible to cover any line annotated as a 'fault of omission'
+    # because there is not a bytecode instruction for any of the candidates
+    return 0
+  fi
+
+  if [ "$pid" == "Closure" ] && [ "$bid" == "103" ]; then
+    # triggering test cases:
+    #   com.google.javascript.jscomp.ControlFlowAnalysisTest::testInstanceOf
+    #   com.google.javascript.jscomp.CheckUnreachableCodeTest::testInstanceOfThrowsException
+    # do not cover faulty class 'com.google.javascript.jscomp.DisambiguateProperties'
+    # and therefore its faulty code, and they do not cover any of the candidate
+    # lines of a missing 'case' of class 'com/google/javascript/jscomp/ControlFlowAnalysis'
+    return 0
+  fi
+
+  if [ "$pid" == "Closure" ] && [ "$bid" == "114" ]; then
+    # there is not a bytecode instruction for the buggy line
+    return 0
+  fi
+
+  if [ "$pid" == "Closure" ] && [ "$bid" == "119" ]; then
+    # there is not a bytecode instruction for the candidate lines
+    return 0
+  fi
+
+  if [ "$pid" == "Lang" ] && [ "$bid" == "29" ]; then
+    # there is not a bytecode instruction for the buggy line
+    return 0
+  fi
+
+  if [ "$pid" == "Lang" ] && [ "$bid" == "56" ]; then
+    # there is not a bytecode instruction for the buggy lines
+    return 0
+  fi
+
+  if [ "$pid" == "Math" ] && [ "$bid" == "12" ]; then
+    # there is not a bytecode instruction for the buggy line or the two lines
+    # annotated as a 'fault of omission'
+    return 0
+  fi
+
+  if [ "$pid" == "Math" ] && [ "$bid" == "104" ]; then
+    # there is not a bytecode instruction for the buggy line
+    return 0
+  fi
+
+  if [ "$pid" == "Mockito" ] && [ "$bid" == "5" ]; then
+    # the test case fails before any line could be cover/executed
+    return 0
+  fi
+
+  if [ "$pid" == "Mockito" ] && [ "$bid" == "8" ]; then
+    # there is not a bytecode instruction for the buggy line
+    return 0
+  fi
+
+  if [ "$pid" == "Time" ] && [ "$bid" == "21" ]; then
+    # the coverage of the second triggering test case (i.e.,
+    # org.joda.time.TestDateTimeZone::testGetShortName_berlin) gets mixed due to
+    # the execution of the other test cases in the same test class. this
+    # behaviour has been double checked with the JaCoCo coverage tool and
+    # therefore can be excluded
+    return 0
+  fi
+
+  return 1
 }
 
 # EOF
